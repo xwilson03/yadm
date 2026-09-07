27b316f53c6fc174075f79deca11a97b167b3f50
xwilson03 <xanderwilson2014@gmail.com>
2026-09-06 19:44:58 -0500

    adds material colorscheme to nvim

---
+++ .config/nvim/lua/plugins/material.lua
@@ -0,0 +1,10 @@
+return {
+    'marko-cerovac/material.nvim',
+    lazy = false,
+    config = function()
+        vim.g.material_style = 'deep ocean'
+        require('material').setup({
+        })
+        vim.cmd('colorscheme material')
+    end,
+}
