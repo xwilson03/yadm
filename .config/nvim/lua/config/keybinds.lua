f5c2acbe83dd50375d4fb6c83ced706bdb8e74ec
xwilson03 <xanderwilson2014@gmail.com>
2026-09-12 17:24:10 -0500

    updates and comments config files

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -1,22 +1,24 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+-- General
 vim.keymap.set("n", "<leader>q", ":qa!<CR>", { desc = "Quit neovim" })
+vim.keymap.set("n", "<leader>w", ":w<CR>",   { desc = "Save buffer" })
 
+-- Navigation
 vim.keymap.set("n", "<leader>h", "<C-w>h", { desc = "Focus left" })
 vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Focus below" })
 vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Focus above" })
 vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Focus right" })
 
-vim.keymap.set("n", "<leader>e", function() require("lib.cycle").close_and_cycle() end, { desc = "Close buffer/terminal" })
-vim.keymap.set("n", "<Tab>", function() require("lib.cycle").cycle("next") end, { desc = "Next buffer/terminal" })
-vim.keymap.set("n", "<S-Tab>", function() require("lib.cycle").cycle("prev") end, { desc = "Previous buffer/terminal" })
+-- Buffers
+vim.keymap.set("n", "<leader>e", function() require("lib.cycle").close_and_cycle() end, { desc = "Close buffer" })
+vim.keymap.set("n", "<Tab>",     function() require("lib.cycle").cycle("next")     end, { desc = "Next buffer" })
+vim.keymap.set("n", "<S-Tab>",   function() require("lib.cycle").cycle("prev")     end, { desc = "Previous buffer" })
+vim.keymap.set("n", "<leader>t", function() vim.cmd("ToggleTerm " .. vim.v.count)  end, { desc = "Toggle Terminal" })
+vim.keymap.set("n", "<leader>d", function() require("lib.diffview").toggle()       end, { desc = "Toggle Diffview" })
+vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>",       { desc = "Toggle file tree" })
 
-vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
-vim.keymap.set("n", "<leader>t", function() vim.cmd("ToggleTerm " .. vim.v.count) end, { desc = "Toggle terminal" })
-
-vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>", { desc = "Toggle file tree" })
-vim.keymap.set("n", "<leader>p", ":Telescope workspaces<CR>", { desc = "Find workspaces" })
-vim.keymap.set("n", "<leader>d", function() require("lib.diffview").toggle() end, { desc = "Toggle Diffview" })
+vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]],             { desc = "Exit terminal mode" })
+vim.keymap.set("n", "<leader>p", ":Telescope workspaces<CR>", { desc = "Search workspaces" })
 
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,12 +1,14 @@
-vim.opt.clipboard = 'unnamedplus'    -- use system clipboard
+local opt = vim.opt
 
-vim.opt.expandtab = true             -- use spaces not tabs
-vim.opt.tabstop = 4                  -- tab width = 4
+opt.clipboard = 'unnamedplus'        -- use system clipboard
 
-vim.opt.number = true                -- show line numbers
-vim.opt.relativenumber = true        -- use relative line numbers
-vim.opt.cursorline = true            -- highlight current line...
-vim.opt.cursorlineopt = "number"       -- ...but just the line number 
+opt.expandtab = true                 -- use spaces not tabs
+opt.tabstop = 4                      -- tab width = 4
 
-vim.o.cmdheight = 0                  -- hide cmdline bar when not in use
+opt.number = true                    -- show line numbers
+opt.relativenumber = true            -- use relative line numbers
+opt.cursorline = true                -- highlight current line...
+opt.cursorlineopt = "number"           -- ...but just the line number
+
+opt.cmdheight = 0                    -- hide cmdline bar when not in use
 require("vim._core.ui2").enable({})  -- enable UI2 (experimental, for cmd/msg/alerts)
