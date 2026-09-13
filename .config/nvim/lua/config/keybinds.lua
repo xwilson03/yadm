403b0918ab4d4d7cc174f48b2b9624eda1130141
xwilson03 <xanderwilson2014@gmail.com>
2026-09-12 22:03:44 -0500

    removes bufferline and buffer cycling

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -11,13 +11,12 @@ vim.keymap.set("n", "<leader>j", "<C-w>j", { desc = "Focus below" })
 vim.keymap.set("n", "<leader>k", "<C-w>k", { desc = "Focus above" })
 vim.keymap.set("n", "<leader>l", "<C-w>l", { desc = "Focus right" })
 
-vim.keymap.set("n", "<Tab>", function() require("lib.buffers").bnext() end, { desc = "Next buffer" })
-vim.keymap.set("n", "<S-Tab>", ":tabnext<CR>", { desc = "Next tab" })
+vim.keymap.set("n", "<Tab>", ":tabnext<CR>", { desc = "Next tab" })
 
 -- Buffers
 vim.keymap.set("n", "<leader>t", ":terminal<CR>i", { desc = "Open Terminal" })
 vim.keymap.set("n", "<leader>g", ":terminal lazygit<CR>i", { desc = "Open Lazygit" })
-vim.keymap.set("n", "<leader>e", function() require("lib.buffers").bdelete() end, { desc = "Close buffer" })
+vim.keymap.set("n", "<leader>e", ":bdelete!<CR>", { desc = "Close buffer" })
 vim.keymap.set("n", "<leader>d", function() require("lib.diffview").toggle() end, { desc = "Toggle Diffview" })
 vim.keymap.set("n", "<leader>n", ":Neotree toggle<CR>", { desc = "Toggle file tree" })
 
