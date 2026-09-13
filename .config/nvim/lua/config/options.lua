4270002a1ce3d4537480be5b9845f9254830d5db
xwilson03 <xanderwilson2014@gmail.com>
2026-09-12 23:21:26 -0500

    updates options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,16 +1,17 @@
 local opt = vim.opt
 
 opt.clipboard = 'unnamedplus'        -- use system clipboard
+opt.undofile = true                  -- persistent undo history
 
 opt.expandtab = true                 -- use spaces not tabs
 opt.tabstop = 4                      -- tab width = 4
-
 opt.wrap = false                     -- disable linewrap
+opt.fcs = 'eob: '                    -- hide '~' chars after end-of-file
 
 opt.number = true                    -- show line numbers
 opt.relativenumber = true            -- use relative line numbers
 opt.cursorline = true                -- highlight current line...
-opt.cursorlineopt = "number"           -- ...but just the line number
+opt.cursorlineopt = 'number'         -- ...but just the line number
 
 opt.cmdheight = 0                    -- hide cmdline bar when not in use
-require("vim._core.ui2").enable({})  -- enable UI2 (experimental, for cmd/msg/alerts)
+require('vim._core.ui2').enable({})  -- enable UI2 (experimental, for cmd/msg/alerts)
