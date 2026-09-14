2a659be6de095c5f261d43b961a0773d5b042891
xwilson03 <xanderwilson2014@gmail.com>
2026-09-14 00:56:38 -0500

    disables coordinates for telescope.buffers

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,8 @@
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { defaults = { initial_mode = "normal" } },
+    opts = {
+        defaults = { initial_mode = "normal" },
+        pickers = { buffers = { disable_coordinates = true } },
+    },
 }
