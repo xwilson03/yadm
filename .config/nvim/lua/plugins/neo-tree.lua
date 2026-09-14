59184ca3d2c500597f98f8673dd5a7fcae107c88
xwilson03 <xanderwilson2014@gmail.com>
2026-09-13 22:11:42 -0500

    fixes neotree open from terminal

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -9,5 +9,14 @@ return {
         sources = { "filesystem" },
         window = { position = "float" },
         filesystem = { filtered_items = { visible = true } },
+        event_handlers = {
+            {
+                event = "file_open_requested",
+                handler = function(args)
+                    vim.api.nvim_set_current_buf(vim.fn.bufadd(args.path))
+                    return { handled = true }
+                end,
+            },
+        },
     },
 }
